ccc2029024c4385047db3a893c1e0460615aef83
Rizzy.stack <rrinshadmt@gmail.com>
2026-04-07 17:09:19 +0530

    Time: 1 ms (81.38%), Space: 58.5 MB (44.96%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -8,9 +8,7 @@ var findWordsContaining = function(words, x) {
 let tot =[]
     for(let i = 0;i < words.length;i++){
         if(words[i].includes(x)){
-
             tot.push(i)
-            
         }
     }
     return tot
