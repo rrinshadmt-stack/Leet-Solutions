42cb71a5dc1eadafb1bad1bbb8cf7efc46da0aa0
Rizzy.stack <rrinshadmt@gmail.com>
2026-04-08 09:45:51 +0530

    Time: 10 ms (33.37%), Space: 62.7 MB (37.7%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+
+    let arr =[];
+
+    for(let i = 0;i<nums.length;i++){
+       arr.push(nums[i]*nums[i])
+
+    }
+    arr.sort((a,b)=>a-b)
+    return arr
+};
