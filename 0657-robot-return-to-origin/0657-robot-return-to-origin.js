96f38712513acc38ffd78c006efdb9a695a6914d
Rizzy.stack <rrinshadmt@gmail.com>
2026-04-10 13:01:17 +0530

    Time: 9 ms (26.61%), Space: 56.7 MB (40.93%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+
+    let x = 0;
+    let y = 0;
+
+for(let move of moves){
+    if(move ==="U"){x++}
+    if(move ==="D"){x--}
+    if(move ==="L"){y++}
+    if(move ==="R"){y--}
+
+
+}
+if(x==0&&y==0){
+
+    return true
+}else{
+    return false
+}
+};
